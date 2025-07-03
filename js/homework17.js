2c96faa3de6cfbf9102e1c94d0cb656cb4beb9ff
Taras1326 <166243411+Taras1326@users.noreply.github.com>
2025-07-04 00:56:21 +0300

    work-17

--- js/homework17.js
+++ js/homework17.js
@@ -1,22 +1,24 @@
 
-     // 1. Зміна тексту кнопки
-     const input = document.getElementById('inputText');
-     const button = document.getElementById('changeBtn');
-     button.addEventListener('click', () => {
-       button.textContent = input.value;
-     });
- 
-     // 2. Зміна зображення (залишаємо, щоб працювало)
-     const image = document.getElementById('mainImage');
-     image.src = 'https://i0.web.de/image/836/40923836,pd=1/lamine-yamal.jpg';
- 
-     // 3. Зміна посилання і alt
-     const linkImg = document.getElementById('linkImage');
-     const link = document.getElementById('customLink2');
-     link.href = 'https://ss.sport-express.ru/userfiles/materials/207/2077795/volga.jpg';
-     linkImg.alt = 'MDN Logo';
- 
-     // 4. Зміна першого елемента списку
-     const list = document.getElementById('myList');
-     const firstItem = list.querySelector('li');
-     firstItem.textContent = 'Nazar';
+
+// 1. Завдання
+const input = document.getElementById("inputText");
+const button = document.getElementById("changeBtn");
+button.addEventListener("click", () => {
+  button.textContent = input.value;
+});
+
+// 2. Завдання
+const image = document.getElementById("mainImage");
+image.src = "https://i0.web.de/image/836/40923836,pd=1/lamine-yamal.jpg";
+
+// 3. Завдання
+const linkImg = document.getElementById("linkImage");
+const link = document.getElementById("customLink2");
+link.href =
+  "https://ss.sport-express.ru/userfiles/materials/207/2077795/volga.jpg";
+linkImg.alt = "MDN Logo";
+
+// 4. Завдання
+const list = document.getElementById("myList");
+const firstItem = list.querySelector("li");
+firstItem.textContent = "Nazar";
